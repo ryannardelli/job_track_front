eb6093a29434a1b95854a5fec00a2e5e3054bbfc
Ryan Nardelli <ryannardelli12@gmail.com>
2026-06-04 21:45:03 -0300

    chore: Creating environment to application front-end

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+   <div className='flex justify-center items-center h-screen'>
+      Hello Vite and Tailwind
+   </div>
+  )
+}
+
+export default App
