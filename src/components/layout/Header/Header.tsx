2283e1908a19fd2435d05c762040c8d4125394db
Ryan Nardelli <ryannardelli12@gmail.com>
2026-06-07 23:04:48 -0300

    fix: Implements fix

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,9 +1,14 @@
 import { ButtonNewJob } from "@/components/ui/Button/ButtonNewJob";
 import { useAuth } from "@/hooks/useAuth";
+import { getInitials } from "@/utils/getInitials";
 import { Plus } from "lucide-react";
 
 export function Header() {
-    const { logout } = useAuth();
+    const { state, logout } = useAuth();
+    const user = state.user;
+
+    const name = user?.name;
+
     const handleLogout = () => {
         logout();
     };
@@ -40,11 +45,11 @@ export function Header() {
           <div className="h-6 w-[1px] bg-slate-300 hidden sm:block" />
 
           <div className="flex items-center gap-3 pl-1 group relative">
-            <div className="w-9 h-9 rounded-full bg-slate-200 border border-slate-300 overflow-hidden flex items-center justify-center font-bold text-sm text-slate-600">
-              JS
+            <div className="w-9 h-9 rounded-full bg-slate-200 border border-slate-300 flex items-center justify-center font-bold text-sm text-slate-600">
+              {getInitials(name)}
             </div>
             <div className="hidden sm:block text-left">
-              <p className="text-xs font-semibold text-slate-800 leading-tight">João Silva</p>
+              <p className="text-xs font-semibold text-slate-800 leading-tight">{name}</p>
               <button 
                 onClick={handleLogout}
                 className="text-[11px] cursor-pointer text-red-500 hover:text-red-700 hover:underline font-medium block text-left transition-colors"
---
+++ src/utils/getInitials.ts
@@ -0,0 +1,10 @@
+export const getInitials = (name?: string) => {
+  if (!name) return "";
+
+  return name
+    .split(" ")
+    .map((n) => n[0])
+    .join("")
+    .toUpperCase()
+    .slice(0, 2);
+};
